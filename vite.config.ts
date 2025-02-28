6f2ba2b7913e5de0f3e788b1981ba55634d4f098
abdallah2340 <138681996+AbdallahMuhammad2@users.noreply.github.com>
2025-02-28 18:48:36 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vertice-site/',    // <<--- AJUSTE IMPORTANTE
-  build: {
-    outDir: 'build',         // se você quer que a pasta final seja "build"
+  optimizeDeps: {
+    exclude: ['lucide-react'],
   },
 });
